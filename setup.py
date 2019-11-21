7c8e3ad17d4c529ecd58a9d1efe3dd2d523fe536
pablodms <pablo_david14@hotmail.com>
2019-11-21 13:56:36 +0100

    Updated pip setup files

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+    name='spacy_spanish_lemmatizer',
+    packages=['spacy_spanish_lemmatizer'],
+    version='0.1',
+    license='MIT',
+    description='Spanish rule-based lemmatization for spaCy',
+    author='Pablo David Muñoz Sánchez',
+    author_email='pablodavid.munoz@gmail.com',
+    url='https://github.com/pablodms/spacy-spanish-lemmatizer',
+    download_url='https://github.com/pablodms/spacy-spanish-lemmatizer/archive/v_0.1.tar.gz',
+    keywords=['Lemmatization', 'spaCy', 'Spanish'],
+    install_requires=['spacy>=2.0'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

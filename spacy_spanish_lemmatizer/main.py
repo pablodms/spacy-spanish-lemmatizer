93df0e4d485a55a55f2c0b3e39d083408410c360
Sebastian Petrausch <sebastian.petrausch@atlasti.com>
2020-04-09 09:51:30 +0200

    use spaCy lemma as fallback instead of token.text

--- spacy_spanish_lemmatizer/main.py
+++ spacy_spanish_lemmatizer/main.py
@@ -5,5 +5,5 @@ class SpacyCustomLemmatizer(object):
     def __call__(self, doc):
         lemmatizer = Lemmatizer()
         for token in doc:
-            token.lemma_ = lemmatizer(token.text, token.pos_)
+            token.lemma_ = lemmatizer(token.text, token.pos_, token.lemma_)
         return doc
